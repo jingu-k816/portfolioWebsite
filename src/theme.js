1556c2241c81fdede8b167c4f103d06e33fcd5ef
Jingu Kang <jinguk0816@gmail.com>
2023-01-19 23:24:17 -0800

    create a custom theme color for the project

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#656565',
+            dark: '#181818',
+            main: '#111111',
+        }
+    }
+})
+
+export default theme;
