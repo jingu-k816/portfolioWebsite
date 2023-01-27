50dcc2d1078ec232064bdca09fb0836f3b524d10
Jingu Kang <jinguk0816@gmail.com>
2023-01-27 02:05:45 -0800

    redesign the makestyles function to use styled div

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,27 +1,22 @@
-import { Box, CssBaseline } from "@mui/material";
-import { makeStyles } from "tss-react/mui";
+import { Box, CssBaseline, styled } from "@mui/material";
 import NavBar from "./navbar";
-import Header from "./header";
-//@ts-ignore
-const useStyles = makeStyles(() => ({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh'
-    },
+
+const Root = styled("div")(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
 }));
-//@ts-ignore
+
 const Layout: React.FC = ({ children }) => {
-    //@ts-ignore
-    const classes = useStyles();
+
     return (
-        //@ts-ignore
-        <Box className={classes.root}>
-            <CssBaseline />
-            <NavBar />
-            <Header />
-            {children}
-        </Box>
+        <Root>
+            <Box>
+                <CssBaseline />
+                <NavBar />
+                {children}
+            </Box>
+        </Root>
     )
 };
 
