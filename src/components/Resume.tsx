8f4f907241552518440c417d3dac61ca2c61a567
Jingu Kang <jinguk0816@gmail.com>
2023-02-05 16:00:40 -0800

    create a resume component

---
+++ src/components/Resume.tsx
@@ -0,0 +1,50 @@
+import { styled } from "@mui/material";
+import Image from "next/image";
+import theme from "@/theme";
+
+// import resume photo
+import resumePic from "@/../public/assets/resume.png"
+
+const Root = styled("div")(({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    maxWidth: "100%",
+    height: "100%",
+    backgroundColor: theme.palette.primary.gray,
+    paddingTop: "15vh",
+    paddingBottom: "5vh",
+    textAlign: "center",
+    color: theme.palette.whites.main,
+    fontSize: "2.5em"
+}));
+
+const DescriptionWrapper = styled("div")(({
+    margin: "2% 20%",
+    textAlign: "center",
+    fontSize: "0.4em",
+}))
+
+const ImageWrapper = styled("div")(({
+    display: "flex",
+    justifyContent: "center",
+    padding: "5vh 0",
+}));
+
+const Resume: React.FC = () => {
+    return (
+        <Root>
+            Resume
+            <DescriptionWrapper>(Click the image to download)</DescriptionWrapper>
+            <ImageWrapper>
+                <Image
+                    src={resumePic}
+                    alt="resume photo"
+                />
+            </ImageWrapper>
+
+        </Root>
+    );
+}
+
+export default Resume;
