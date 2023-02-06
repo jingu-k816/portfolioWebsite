c3cefa56cee7a9154e479b0d3929ad267f01b868
Jingu Kang <jinguk0816@gmail.com>
2023-02-05 21:28:03 -0800

    update next config for users to download the resume

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.pdf$/i,
+      type: 'asset/resource',
+    })
+
+    return config
+  }
 }
 
 module.exports = nextConfig
